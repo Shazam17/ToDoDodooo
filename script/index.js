8bff241f5a0745dae31e78c635cfe08fb57c9edd
Danny <112708137+openglobalmain@users.noreply.github.com>
2023-02-07 19:35:13 +0700

    FINISH

--- script/index.js
+++ script/index.js
@@ -1,41 +1,38 @@
 const requestURL = "https://dummyjson.com/products";
-//Счетчик объектов
+let listPlace = 1;
+//Счетчик объектов для перемещения description
 var parent = document.getElementsByClassName("list")[0];
 parent.onmouseover = function (e) {
     var e = e || event;
     var target = e.target || e.srcElement;
-    for(var i = 0; i < parent.children.length; i++) {
-        if(parent.children[i] == target) console.log(i); return i;
+    for (var i = 0; i < parent.children.length; i++) {
+        if (parent.children[i] == target) {listPlace = i;}
     }
 }
-
+//Отдельная ф-ия для стартового запроса
 function request() {
     let dataBase = sendRequest('GET', requestURL);
 
     dataBase.then(data => {
-        console.log(data.products[0].title)
         const title = [];
         for (let i = 0; i < 10; i++) {
             title[i] = data.products[i].title;
-            console.log(data.products[i].title);
         }
-        startAddListItem(title);
+        addListItem(title, 10);
         dataBase.catch(err => console.log(err))
     })
 }
 request();
-
+//Ф-я для создания заданного кол-ва элементов
 function addRequest() {
     let dataBase = sendRequest('GET', requestURL);
     let Quantity = document.getElementById('Quant').value;
     if (Quantity > 30) { alert("Let me number lower than 30") }
     else {
         dataBase.then(data => {
-            console.log(data.products[0].title)
             const title = [];
             for (let i = 0; i < Quantity; i++) {
                 title[i] = data.products[i].title;
-                console.log(data.products[i].title);
             }
             addListItem(title, Quantity);
             dataBase.catch(err => console.log(err))
@@ -43,11 +40,13 @@ function addRequest() {
     }
 }
 
+function addSortedRequest(){
+
+}
+//Создание описания
 function addRequestDescription(n) {
     let dataBase = sendRequest('GET', requestURL);
-    let Quantity = document.getElementById('Quant').value;
     dataBase.then(data => {
-        console.log(data.products[0].title);
         const id = [];
         const title = [];
         const description = [];
@@ -75,62 +74,64 @@ function addRequestDescription(n) {
         }
 
         let removeDiv = document.querySelectorAll(".listObjectDescription");
-        for (let k = removeDiv.length; k > 0; k--) {
-            removeDiv[k - 1].remove();
-        }
         let targetDiv = document.querySelector(".mainContainer");
         let newDiv = document.createElement("div");
+        for (let k = removeDiv.length; k > 0; k--) { removeDiv[k - 1].remove(); }
         newDiv.className = 'listObjectDescription';
         newDiv.id = 'Description';
         newDiv.innerHTML = ' <button id = "x" onclick = removeDescriptionX()>x</button> <img src="' + images[n][0] + '" id="big"> <div id="thumbs"> <a href="' + images[n][0] + '"> <img src="' + images[n][0] + '"> </a> <a href="' + images[n][1] + '"> <img src="' + images[n][1] + '"> </a> <a href="' + images[n][2] + '"> <img src="' + images[n][2] + '"> <a href="' + images[n][3] + '"> <img src="' + images[n][3] + '"> <a href="' + images[n][4] + '"> <img src="' + images[n][4] + '"> </a> </div> <div class="mainDescriptionInfo"><div>id: ' + id[n] + '</div> <div>title: ' + title[n] + '</div> <div>description: "' + description[n] + '"</div> <div>price: ' + price[n] + '</div> <div>discountPercentage: ' + discountPercentage[n] + '</div> <div>rating: ' + rating[n] + '</div> <div>stock: ' + stock[n] + '</div> <div>brand: "' + brand[n] + '"</div> <div>category: "' + category[n] + '"</div> </div>';
         targetDiv.append(newDiv);
 
-        changeObject(n,newDiv);
+        changeObject(n, newDiv);
+
+        changeItem(listPlace);
 
         imgChanging();
 
         dataBase.catch(err => console.log(err))
+
+
     })
 }
-
+//Запрос на сервер
 function sendRequest(method, url, body = null) {
     return fetch(url).then(response => {
         return response.json()
     })
 }
-
+//Перемещение Description
 function changeItem(h) {
 
     document.getElementById('Description').style.backgroundColor = "rgba(102, 51, 153, 0.297)";
-    document.getElementById('Description').style.gridArea = h + 2 + '/2/' + (h + 11) + '/3';
-    
-}
+    document.getElementById('Description').style.gridArea = h + 3 + '/2/' + (h + 12) + '/3';
 
-function changeObject(n,description){
+}
+//Св-ва объектов Листа
+function changeObject(n, description) {
     let object = document.getElementById(n);
-        description.addEventListener('mouseover', function() {
-        object.style.backgroundColor = "rgba(102, 51, 153, 0.297)";});
-        description.addEventListener('mouseout', function() {
-        object.style.backgroundColor = "";});
-        document.getElementById('x').addEventListener('click', function() {
-        object.style.backgroundColor = "";});
-        
-}
+    description.addEventListener('mouseover', function () {
+        object.style.backgroundColor = "rgba(102, 51, 153, 0.297)";
+    });
+    description.addEventListener('mouseout', function () {
+        object.style.backgroundColor = "";
+    });
+    document.getElementById('x').addEventListener('click', function () {
+        object.style.backgroundColor = "";
+    });
 
-function startAddListItem(title) {
-    removeListItems();
-    for (let j = 0; j < 10; j++) {
+}
+function addSortedListItem(title, i){
         let targetDiv = document.querySelector(".list");
         let newDiv = document.createElement("div");
         newDiv.className = 'listObject';
-        newDiv.id = j;
-        newDiv.onmouseover = () => addRequestDescription(j);
+        newDiv.id = i;
+        newDiv.onmouseover = () => addRequestDescription(newDiv.id);
         newDiv.draggable = 'true';
-        newDiv.innerHTML = '<a>' + title[j] + '</a>';
+        newDiv.innerHTML = '<a>' + title[i] + '</a>';
         targetDiv.append(newDiv);
     }
-}
 
+//Добавление объектов листа
 function addListItem(title, Quantity) {
     removeListItems();
     for (let i = 0; i < Quantity; i++) {
@@ -138,13 +139,13 @@ function addListItem(title, Quantity) {
         let newDiv = document.createElement("div");
         newDiv.className = 'listObject';
         newDiv.id = i;
-        newDiv.onmouseover = () => addRequestDescription(i);
+        newDiv.onmouseover = () => addRequestDescription(newDiv.id);
         newDiv.draggable = 'true';
         newDiv.innerHTML = '<a>' + title[i] + '</a>';
         targetDiv.append(newDiv);
     }
 }
-
+//Чистка объектов листа
 function removeListItems() {
     const removeItems = document.querySelectorAll(".listObject");
     const removeDescription = document.querySelector(".listObjectDescription");
@@ -156,82 +157,128 @@ function removeListItems() {
         removeDescription.remove();
     }
 }
-
-function removeDescriptionX(){
+//Закрытие с крестика
+function removeDescriptionX() {
     const removeDescription = document.querySelector(".listObjectDescription");
     if (removeDescription == null) { }
     else {
         removeDescription.remove();
     }
 }
-
-function imgChanging(){
+//Галерея фото в Description
+function imgChanging() {
     var thumbs = document.querySelectorAll('#thumbs > a');
     var big = document.getElementById('big');
     for (var i = 0; i < thumbs.length; i++) {
-        thumbs[i].addEventListener('mouseover', function(e) {
+        thumbs[i].addEventListener('mouseover', function (e) {
             e.preventDefault();
             big.src = this.href;
-        });}
+        });
+    }
 }
-//Создаем подвижность наших объектов
+//Создаем возможность перетаскивания наших объектов 
+function dragAndDrop() {
+    const listObjects = document.querySelectorAll('.listObject');
+    const list = document.querySelectorAll('.list');
+    const tasksListElement = document.querySelector(`.list`);
+    const taskElements = tasksListElement.querySelectorAll(`.listObject`);
 
-const listObjects = document.querySelectorAll('.listObject');
-const list = document.querySelectorAll('.list');
+    for (const task of taskElements) { task.draggable = true; }
 
+    tasksListElement.addEventListener(`dragstart`, (evt) => { evt.target.classList.add(`selected`); });
+    tasksListElement.addEventListener(`dragend`, (evt) => { evt.target.classList.remove(`selected`); });
 
+    const getNextElement = (cursorPosition, currentElement) => {
+        const currentElementCoord = currentElement.getBoundingClientRect();
+        const currentElementCenter = currentElementCoord.y + currentElementCoord.height / 2;
+        const nextElement = (cursorPosition < currentElementCenter) ?
+            currentElement :
+            currentElement.nextElementSibling;
+        return nextElement;
+    };
 
-function dragAndDrop(){
-const tasksListElement = document.querySelector(`.list`);
-const taskElements = tasksListElement.querySelectorAll(`.listObject`);
+    tasksListElement.addEventListener(`dragover`, (evt) => {
+        evt.preventDefault();
 
-for (const task of taskElements) {
-  task.draggable = true;
-}
+        const activeElement = tasksListElement.querySelector(`.selected`);
+        const currentElement = evt.target;
+        const isMoveable = activeElement !== currentElement &&
+            currentElement.classList.contains(`listObject`);
 
-tasksListElement.addEventListener(`dragstart`, (evt) => {
-  evt.target.classList.add(`selected`);
-});
+        if (!isMoveable) {
+            return;
+        }
 
-tasksListElement.addEventListener(`dragend`, (evt) => {
-  evt.target.classList.remove(`selected`);
-});
+        const nextElement = getNextElement(evt.clientY, currentElement);
+        if (nextElement && activeElement === nextElement.previousElementSibling || activeElement === nextElement) { return; }
 
-const getNextElement = (cursorPosition, currentElement) => {
-  const currentElementCoord = currentElement.getBoundingClientRect();
-  const currentElementCenter = currentElementCoord.y + currentElementCoord.height / 2;
-  
-  const nextElement = (cursorPosition < currentElementCenter) ?
-    currentElement :
-    currentElement.nextElementSibling;
-  
-  return nextElement;
-};
-
-tasksListElement.addEventListener(`dragover`, (evt) => {
-  evt.preventDefault();
-  
-  const activeElement = tasksListElement.querySelector(`.selected`);
-  const currentElement = evt.target;
-  const isMoveable = activeElement !== currentElement &&
-    currentElement.classList.contains(`listObject`);
-    
-  if (!isMoveable) {
-    return;
-  }
-  
-  const nextElement = getNextElement(evt.clientY, currentElement);
-  if (nextElement && activeElement === nextElement.previousElementSibling || activeElement === nextElement) {return;}
-    console.log(activeElement.id,nextElement.id);
-
-	tasksListElement.insertBefore(activeElement, nextElement);
-});
+        tasksListElement.insertBefore(activeElement, nextElement);
+    });
 }
 dragAndDrop();
+//sortByPrice
+function  sortByPrice(){
+    let Quantity = parent.children.length;
+    let dataBase = sendRequest('GET', requestURL);
+    dataBase.then(data => {
+        let price = [];
+        const title = [];
+        let sortedListCounter = [];
+        
+        for(let i = 0;i < Quantity;i++){title[i] = data.products[i].title;}        
+        for(let i = 0;i < Quantity;i++){price[i] = data.products[i].price;}
+        
+        price.sort(function(a,b){return a-b;});
+        for(let i = 0;i < Quantity;i++){
 
+            for(let j = 0;j < Quantity;j++){
 
+                if((price[i]==data.products[j].price) && (sortedListCounter[i-1] != (data.products[j].id-1)))
+                {
+                    sortedListCounter[i]=(data.products[j].id-1);
+                    console.log(title[i],'    ',data.products[j].title);
+                    console.log(sortedListCounter);}
 
+                else{}
+            }}
 
+        removeListItems();
 
-
-//sort 
+            for(let i = 0; i < Quantity;  i++){
+            addSortedListItem(title,sortedListCounter[i]);}
+            
+        console.log(price);
+});}
+//sortByRating
+function sortByRating(){
+        let Quantity = parent.children.length;
+        let dataBase = sendRequest('GET', requestURL);
+        dataBase.then(data => {
+            let rating = [];
+            const title = [];
+            let sortedListCounter = [];
+            
+            for(let i = 0;i < Quantity;i++){title[i] = data.products[i].title;}        
+            for(let i = 0;i < Quantity;i++){rating[i] = data.products[i].rating;}
+            
+            rating.sort(function(a,b){return a-b;});
+            for(let i = 0;i < Quantity;i++){
+    
+                for(let j = 0;j < Quantity;j++){
+    
+                    if((rating[i]==data.products[j].rating) && (sortedListCounter[i-1] != (data.products[j].id-1)))
+                    {
+                        sortedListCounter[i]=(data.products[j].id-1);
+                        console.log(title[i],'    ',data.products[j].title);
+                        console.log(sortedListCounter);}
+    
+                    else{}
+                }}
+    
+            removeListItems();
+    
+                for(let i = 0; i < Quantity;  i++){
+                addSortedListItem(title,sortedListCounter[i]);}
+                
+            console.log(rating);
+    });}
